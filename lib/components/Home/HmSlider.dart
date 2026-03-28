4a931815f7e7b78fe458709b881df65e04980f45
jinyi <2577435721@qq.com>
2026-03-28 14:20:09 +0800

    feat :新增轮播图搜索框和指示灯

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -11,12 +11,74 @@ class HmSlider extends StatefulWidget {
 }
 
 class _HmSliderState extends State<HmSlider> {
+  CarouselSliderController buttonCarouselController =
+      CarouselSliderController(); //控制轮播图跳转的控制器
+  int _currentIndex = 0; //当前选中的轮播图索引
+  /// 搜索框
+  Widget _getSearch() {
+    //container div+style
+    return Positioned(
+      top: 10,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          height: 50,
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(0, 0, 0, 0.4),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          child: Text('搜索', style: TextStyle(color: Colors.white)),
+        ),
+      ),
+    );
+  }
+
+  //返回指示灯导航组件
+  Widget _getDots() {
+    //container div+style
+    return Positioned(
+      bottom: 10,
+      left: 0,
+      right: 0,
+      child: SizedBox(
+        height: 40,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center, //主轴居中
+          children: List.generate(
+            widget.bannerList.length,
+            (int index) => GestureDetector(
+              onTap: () {
+                buttonCarouselController.jumpToPage(index);
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                width: index == _currentIndex ? 40 : 20,
+                height: 6,
+                margin: EdgeInsets.symmetric(horizontal: 4),
+                decoration: BoxDecoration(
+                  color: index == _currentIndex?Colors.white:Color.fromRGBO(0, 0, 0, 0.3),
+                  borderRadius: BorderRadius.circular(3),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   Widget _getSlider() {
     //在flutter中获取屏幕宽度的方法：MediaQuery.of(context).size.width
     double screenWidth = MediaQuery.of(context).size.width;
     // 返回轮播插件
     //根据数据渲染不同的轮播选项
     return CarouselSlider(
+      carouselController: buttonCarouselController,
       items: List.generate(
         widget.bannerList.length,
         (int index) => (Image.network(
@@ -25,13 +87,23 @@ class _HmSliderState extends State<HmSlider> {
           width: screenWidth,
         )),
       ),
-      options: CarouselOptions(viewportFraction: 1, autoPlay: true, autoPlayInterval:  Duration(seconds: 5),height: 300,),
+      options: CarouselOptions(
+        viewportFraction: 1,
+        autoPlay: true,
+        // autoPlayInterval: Duration(seconds: 5),
+        height: 300,
+        onPageChanged: (index, reason) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     //strack ->轮播图 搜索框 指示灯导航
-    return Stack(children: [_getSlider()]);
+    return Stack(children: [_getSlider(), _getSearch(), _getDots()]);
   }
 }
